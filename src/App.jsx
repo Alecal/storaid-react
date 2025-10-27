c0c6fb24b8e002d1b27c7cca64b8e5cbbd41a9cd
Alexander Nyman <stalander@gmail.com>
2025-10-27 15:11:48 +0100

    Fixat key felmeddelande i Socialsbadge samt felaktig name class istället för className

--- src/App.jsx
+++ src/App.jsx
@@ -25,34 +25,34 @@ const App = () => {
     <Header/>
     <Hero/>
     <section id="aboutUs" className='mx-auto max-w-7xl py-10'>
-      <div class="flex flex-col md:flex-row gap-6">
-      <div class="w-full md:w-1/2 py-4">
+      <div className="flex flex-col md:flex-row gap-6">
+      <div className="w-full md:w-1/2 py-4">
           <AboutUsCard/>
       </div>
-      <div class="w-full md:w-1/2 py-4">
+      <div className="w-full md:w-1/2 py-4">
         <p className="text-primary-bold">About Us</p>
         <h4 className="text-green leading-tight mb-5">Providing a Safe Space for Your Treasured Items</h4>
         <p className='mb-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi, non sodales augue. Phasellus eget elit gravida.</p>
 
-      <div class="flex gap-6 mb-10">
-        <div class="w-1/2">
+      <div className="flex gap-6 mb-10">
+        <div className="w-1/2">
           <Tile title="Vision">
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusan tium doloremque laudantium.
           </Tile>
         </div>
 
-        <div class="w-1/2">
+        <div className="w-1/2">
           <Tile title="Mission">
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusan tium doloremque laudantium.
           </Tile>
         </div>
       </div>
-      <div class="flex">
-        <div class="w-1/2">
+      <div className="flex">
+        <div className="w-1/2">
           <ButtonStandard label="Discover More"/>
         </div>
 
-        <div class="w-1/2">
+        <div className="w-1/2">
           <InfoBadge icon={faPhone} title="More information" subTitle="+46 8 123 122 44">Tjena</InfoBadge>
         </div>
       </div>
--- src/components/SocialsBadge.jsx
+++ src/components/SocialsBadge.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// Import all necessary icon assets
 import facebookIcon from '../assets/icons/facebook.svg';
 import twitterIcon from '../assets/icons/x-twitter.svg';
 import instagramIcon from '../assets/icons/instagram.svg';
@@ -14,29 +13,25 @@ const icons = {
 };
 
 function SocialsBadge({ names }) {
-
   if (!Array.isArray(names) || names.length === 0) {
     return null;
   }
 
   return (
     <div className="socialsContainer">
-      
-        {names.map((name, index) => {
-          const iconSrc = icons[name]
-          return (
-
-            
-            <div className="socialsBadge">
+      {names.map((name) => {
+        const iconSrc = icons[name];
+        
+        return (
+          <div key={name} className="socialsBadge">
             <img
-              key={name} 
               className="socialsIcon"
               src={iconSrc}
               alt={`${name} icon`}
             />
-            </div>
-          );
-        })}
+          </div>
+        );
+      })}
     </div>
   );
 }

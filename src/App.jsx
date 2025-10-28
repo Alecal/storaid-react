ee9668300f7bd82fb7d865b6f7f8f671e330980f
Alexander Nyman <stalander@gmail.com>
2025-10-28 11:16:58 +0100

    Lagt till Book Now section

--- src/App.jsx
+++ src/App.jsx
@@ -120,6 +120,13 @@ const App = () => {
             </div>
         </div>
     </section>
+    <section id="bookNow" className='sectionGreenLight'>
+        <div className='text-center mx-auto max-w-5xl py-20'>
+            <Headline color="text-white" size="h3">Looking for Secure and Flexible Storage? Find Your Perfect Fit With Us.</Headline>
+            <p className='body-standard-text text-white pb-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.</p>
+            <ButtonStandard mode="light">Book Now</ButtonStandard>
+        </div>
+    </section>
     </>
   )
 }

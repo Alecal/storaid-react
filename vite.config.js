339d02462f89978c37d6b4cd544b58ec8fc8d0cb
Alexander Nyman <stalander@gmail.com>
2025-11-10 17:09:52 +0100

    Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/storaid-react/',
+  base: '/',
   plugins: [
     react({
       babel: {
